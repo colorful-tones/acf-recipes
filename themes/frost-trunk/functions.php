a8b9cd891df20fa1b3245c74a743aba3fdcf8b6d
Damon Cook <damonacook@gmail.com>
2023-03-15 16:45:54 -0400

    ‼️ BREAKING: forgot to add /frost-trunk 🤦‍♂️

---
+++ themes/frost-trunk/functions.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * This file adds functions to the Frost WordPress theme.
+ *
+ * @package Frost
+ * @author  WP Engine
+ * @license GNU General Public License v2 or later
+ * @link    https://frostwp.com/
+ */
+
+if ( ! function_exists( 'frost_setup' ) ) {
+
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 *
+	 * @since 0.8.0
+	 *
+	 * @return void
+	 */
+	function frost_setup() {
+
+		// Make theme available for translation.
+		load_theme_textdomain( 'frost', get_template_directory() . '/languages' );
+
+		// Enqueue editor styles and fonts.
+		add_editor_style(
+			array(
+				'./style.css',
+			)
+		);
+
+		// Remove core block patterns.
+		remove_theme_support( 'core-block-patterns' );
+
+	}
+}
+add_action( 'after_setup_theme', 'frost_setup' );
+
+// Enqueue style sheet.
+add_action( 'wp_enqueue_scripts', 'frost_enqueue_style_sheet' );
+function frost_enqueue_style_sheet() {
+
+	wp_enqueue_style( 'frost', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+
+}
+
+/**
+ * Register block styles.
+ *
+ * @since 0.9.2
+ */
+function frost_register_block_styles() {
+
+	$block_styles = array(
+		'core/columns' => array(
+			'columns-reverse' => __( 'Reverse', 'frost' ),
+		),
+		'core/list' => array(
+			'no-disc' => __( 'No Disc', 'frost' ),
+		),
+		'core/navigation-link' => array(
+			'outline' => __( 'Outline', 'frost' ),
+		),
+		'core/social-links' => array(
+			'outline' => __( 'Outline', 'powder' ),
+		),
+	);
+
+	foreach ( $block_styles as $block => $styles ) {
+		foreach ( $styles as $style_name => $style_label ) {
+			register_block_style(
+				$block,
+				array(
+					'name'  => $style_name,
+					'label' => $style_label,
+				)
+			);
+		}
+	}
+}
+add_action( 'init', 'frost_register_block_styles' );
---
+++ themes/frost-trunk/patterns/slide-title.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Slide [title]
+ * Slug: slide-title
+ * Description: 
+ * Categories: 
+ * Keywords: 
+ * Viewport Width: 1280
+ * Block Types: core/post-content
+ * Post Types: presentation
+ * Inserter: true
+ */
+
+?>
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/patterns/images/WPE-Splash-1.webp","id":233,"dimRatio":0,"focalPoint":{"x":0,"y":0},"minHeight":100,"minHeightUnit":"vh","align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"typography":{"fontSize":"2rem"}}} -->
+<div class="wp-block-cover alignfull" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small);font-size:2rem;min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-233" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/patterns/images/WPE-Splash-1.webp" style="object-position:0% 0%" data-object-fit="cover" data-object-position="0% 0%"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|neutral"}}},"typography":{"fontSize":"3rem"}},"textColor":"base","layout":{"type":"constrained","justifyContent":"left","contentSize":"72vw"}} -->
+<div class="wp-block-group has-base-color has-text-color has-link-color" style="font-size:3rem"><!-- wp:paragraph -->
+<p>Slide title</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"left","placeholder":"Write title…","fontSize":"large"} -->
+<p class="has-text-align-left has-large-font-size">Slide text goes here...</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
